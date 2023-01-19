797f2ac1630017b4e2fb0139d941479b4d5caa83
RBitca88 <radubitca4@gmail.com>
2023-01-19 23:08:38 +0200

    Modified file Hello.java.

--- Hello.java
+++ Hello.java
@@ -1,7 +1,13 @@
+import java.util.Scanner;
+
 public class Hello {
 	
 	public static void main(String[] args) {
-		
-		System.out.println("Hello world!");
+ 
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Please write something thet you what to be wtiten to the screen -> ");
+		String input = sc.nextLine();
+		System.out.println("Hello class!" + input);
+		System.out.println("What a wonderfull day!");
 	}
 }
